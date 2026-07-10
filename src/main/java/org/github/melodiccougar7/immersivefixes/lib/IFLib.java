ae8c6eb46cb6ca82d3ae35a400905627331d5fc9
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-07-10 12:46:35 -0600

    Add logger

---
+++ src/main/java/org/github/melodiccougar7/immersivefixes/lib/IFLib.java
@@ -0,0 +1,9 @@
+package org.github.melodiccougar7.immersivefixes.lib;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public class IFLib {
+    public static final Logger IF_LOGGER = LogUtils.getLogger();
+}
+
