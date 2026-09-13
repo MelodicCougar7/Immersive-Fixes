c165b4039dcf8ba9bd6daacd31b20962142dc1a9
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-09-12 20:33:13 -0600

    Fix projectile sound

---
+++ src/main/java/org/github/melodiccougar7/immersivefixes/mixin/common/IEProjectileEntityMixin.java
@@ -0,0 +1,20 @@
+package org.github.melodiccougar7.immersivefixes.mixin.common;
+
+import org.github.melodiccougar7.immersivefixes.lib.IFLib;
+
+import blusunrize.immersiveengineering.common.entities.IEProjectileEntity;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = IEProjectileEntity.class, remap = false)
+public abstract class IEProjectileEntityMixin {
+    @Inject(method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/level/Level;)V", at = @At("RETURN"))
+    private void immersiveFixes$silenceArrowHitSound(CallbackInfo ci) {
+        IFLib.logMixinActive("IEProjectileEntityMixin");
+        ((AbstractArrow) (Object) this).setSoundEvent(SoundEvents.EMPTY);
+    }
+}
