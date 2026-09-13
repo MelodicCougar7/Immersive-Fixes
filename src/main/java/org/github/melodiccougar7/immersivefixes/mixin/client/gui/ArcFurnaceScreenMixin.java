2059a9c9c01d36732128d0f35a0adab110a0e11b
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-09-12 20:35:59 -0600

    Fix tooltips staying

---
+++ src/main/java/org/github/melodiccougar7/immersivefixes/mixin/client/gui/ArcFurnaceScreenMixin.java
@@ -0,0 +1,22 @@
+package org.github.melodiccougar7.immersivefixes.mixin.client.gui;
+
+import org.github.melodiccougar7.immersivefixes.lib.IFLib;
+
+import blusunrize.immersiveengineering.client.gui.ArcFurnaceScreen;
+import blusunrize.immersiveengineering.client.gui.elements.GuiButtonIE;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = ArcFurnaceScreen.class, remap = false)
+public abstract class ArcFurnaceScreenMixin {
+    @WrapOperation(method = "gatherAdditionalTooltips(IILjava/util/function/Consumer;Ljava/util/function/Consumer;)V", at = @At(value = "INVOKE", target = "Lblusunrize/immersiveengineering/client/gui/elements/GuiButtonIE;isHoveredOrFocused()Z", remap = true))
+    private boolean immersiveFixes$hoveredOnly(GuiButtonIE button, Operation<Boolean> original) {
+        IFLib.logMixinActive("ArcFurnaceScreenMixin");
+        if (button.isHovered()) {
+            ((ArcFurnaceScreen) (Object) this).getMenu().getCarried().isEmpty();
+        }
+        return false;
+    }
+}
